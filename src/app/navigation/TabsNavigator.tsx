820ce0fdf1f767b8579b0148dc94ee8243b0bbf3
Arsen <askhatuly@gmail.com>
2026-02-08 20:00:44 +0500

    feat(tabs): icons for tabs

--- src/app/navigation/TabsNavigator.tsx
+++ src/app/navigation/TabsNavigator.tsx
@@ -1,4 +1,6 @@
+import type { ComponentProps } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import { FavoritesScreen } from '../../pages/favorites/ui/FavoritesScreen';
 import { NewsPage } from '../../pages/news/ui/NewsPage';
@@ -9,7 +11,33 @@ const Tab = createBottomTabNavigator<BottomTabParamList>();
 
 export function TabsNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size, focused }) => {
+          const iconMap: Record<
+            keyof BottomTabParamList,
+            {
+              focused: ComponentProps<typeof Ionicons>['name'];
+              unfocused: ComponentProps<typeof Ionicons>['name'];
+            }
+          > = {
+            News: { focused: 'newspaper', unfocused: 'newspaper-outline' },
+            Favorites: { focused: 'heart', unfocused: 'heart-outline' },
+            Settings: { focused: 'settings', unfocused: 'settings-outline' },
+          };
+
+          const icons = iconMap[route.name as keyof BottomTabParamList];
+
+          return (
+            <Ionicons
+              name={focused ? icons.focused : icons.unfocused}
+              size={size}
+              color={color}
+            />
+          );
+        },
+      })}
+    >
       <Tab.Screen name="News" component={NewsPage} />
       <Tab.Screen name="Favorites" component={FavoritesScreen} />
       <Tab.Screen name="Settings" component={SettingsAuthScreen} />
